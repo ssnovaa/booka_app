fa696aeefa12ea7a068b9a9e33920393fa9f5964
ssnovaa <ssnovaa@gmail.com>
2025-11-15 18:25:02 +0200

    chat15

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -7,7 +7,7 @@ import 'package:flutter/foundation.dart' show kDebugMode, debugPrint;
 import 'package:provider/provider.dart';
 import 'package:characters/characters.dart';
 
-// app
+// app а
 import 'package:booka_app/constants.dart';
 import 'package:booka_app/widgets/current_listen_card.dart';
 import 'package:booka_app/user_notifier.dart';
